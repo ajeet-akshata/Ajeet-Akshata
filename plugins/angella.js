81efd0aa09e97a22db63b50f2288dc8c5fa3aa2b
ajeet-akshata <102581909+ajeet-akshata@users.noreply.github.com>
2022-03-29 14:32:52 +0530

    Update angella.js

--- plugins/angella.js
+++ plugins/angella.js
@@ -32,11 +32,11 @@ Asena.addCommand({ pattern: 'ig ?(.*)', fromMe: false, desc: IG_DESC}, async (me
         const msg = `${type}`
 
 	 if (msg === 'image') { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, {quoted: message.data}, {
-          caption: "Copyright © 2022 | ᳆⃞🇱angella↯ǫᴇᴇɴᬐ"
+          caption: "Copyright © 2022 | 🅤𝚗𝚔𝚗𝚘𝚠𝚗 🅗𝚊𝚌𝚔𝚎࿐"
         })}
 		 	 
 	if (msg === 'video') { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.video, {quoted: message.data}, {
-          caption: "Copyright © 2022 | ᳆⃞angells↯ǫᴇᴇɴᬐ"
+          caption: "Copyright © 2022 | 🅤𝚗𝚔𝚗𝚘𝚠𝚗 🅗𝚊𝚌𝚔𝚎࿐"
         })}
 	
         
@@ -71,7 +71,7 @@ Asena.addCommand({ pattern: 'fb ?(.*)', fromMe: false, desc: FBDESC }, async (me
         const msg = `*${CAPTION}*: ${judul}`
 
         await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.video, {quoted: message.data}, {
-          caption: "Copyright © 2022 | ᳆⃞angella↯ǫᴇᴇɴᬐ"
+          caption: "Copyright © 2022 | 🅤𝚗𝚔𝚗𝚘𝚠𝚗 🅗𝚊𝚌𝚔𝚎࿐"
         })
       })
       .catch(
